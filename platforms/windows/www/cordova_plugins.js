95aa95bd403281f5997a88acc96e126398b6de6d
Nageshwar <njha.cse@gmail.com>
2016-04-01 11:01:07 +0530

    1st commit
    first and last

---
+++ platforms/windows/www/cordova_plugins.js
@@ -0,0 +1,27 @@
+﻿cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/phonegap-plugin-push/www/push.js",
+        "id": "phonegap-plugin-push.PushNotification",
+        "pluginId": "phonegap-plugin-push",
+        "clobbers": [
+            "PushNotification"
+        ]
+    },
+    {
+        "file": "plugins/phonegap-plugin-push/src/windows/PushPluginProxy.js",
+        "id": "phonegap-plugin-push.PushPlugin",
+        "pluginId": "phonegap-plugin-push",
+        "merges": [
+            ""
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-whitelist": "1.2.1",
+    "phonegap-plugin-push": "1.6.1"
+}
+// BOTTOM OF METADATA
+});
